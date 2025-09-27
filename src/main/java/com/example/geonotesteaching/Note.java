991f34cfc5d544f91c4e5109eeeebafca94a284e
jesuuslopeez <jlopezp@iesrafaelalberti.es>
2025-09-28 00:04:44 +0200

    Validación de carácteres título, control de espacios con trim() y control de null/empty

--- src/main/java/com/example/geonotesteaching/Note.java
+++ src/main/java/com/example/geonotesteaching/Note.java
@@ -8,7 +8,15 @@ import java.time.Instant;
 public record Note(long id, String title, String content, GeoPoint location, Instant createdAt, Attachment attachment) {
     public Note {
         if (title == null || title.isBlank()) throw new IllegalArgumentException("title requerido");
-        if (content == null) content = "";
+        if (title.length() < 3) throw new IllegalArgumentException("El titulo debe tener mínimo 3 carácteres");
+        if (content == null) {
+            content = "-";
+        } else {
+            content = content.trim();
+            if (content.isEmpty()) {
+                content = "-";
+            }
+        }
         if (location == null) throw new IllegalArgumentException("location requerido");
         if (createdAt == null) createdAt = Instant.now();
     }
